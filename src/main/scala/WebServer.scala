9e47a796c2b286b113605ed7219adc7df1f4afe1
akahorisoma <akapo.kk.i@gmail.com>
2020-05-01 13:10:42 +0900

    feat basic認証機能の実装

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -2,7 +2,9 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.directives.Credentials
 import akka.stream.ActorMaterializer
+import com.typesafe.config.{Config, ConfigException, ConfigFactory}
 
 import scala.io.StdIn
 
@@ -14,14 +16,24 @@ object WebServer {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
     val route =
-      path("hello") {
-        get {
-          extractHost { hostName =>
-            system.log.info(hostName)
-            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      pathPrefix("hello") {
+        concat(
+          pathEnd{
+            get {
+              extractHost { hostName =>
+                system.log.info(hostName)
+                complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+              }
+            }
+          },
+          path(IntNumber) { int =>
+            authenticateBasic(realm = "secure site", myUserPassAuthenticator) { userName =>
+              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>Say hello to $userName $int </h1>"))
+            }
           }
-        }
+        )
       }
+
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
@@ -31,4 +43,21 @@ object WebServer {
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
   }
+
+  def myUserPassAuthenticator(credentials: Credentials): Option[String] = {
+    val config: Config = ConfigFactory.load()
+    credentials match {
+      case p @ Credentials.Provided(id) =>
+        try {
+          val password = config.getString(id + ".password")
+          if (p.verify(password)) Some(id)
+          else None
+        } catch {
+          case _: ConfigException.Missing => None
+          case _: ConfigException.WrongType => None
+        }
+      case _ => None
+    }
+  }
+
 }

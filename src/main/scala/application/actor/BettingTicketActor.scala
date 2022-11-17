d2af6162efdb858f419a26e38afa97a626af8047
akahori-soma <akahori.soma@gmail.com>
2022-11-17 10:45:06 +0900

    feat Actorの状態遷移機能の追加

--- src/main/scala/application/actor/BettingTicketActor.scala
+++ src/main/scala/application/actor/BettingTicketActor.scala
@@ -18,40 +18,63 @@ object BettingTicketActor {
   sealed trait Event
   case class Saved(normalWinBettingTicketPayload: NormalWinBettingTicketPayload) extends Event
 
-  final case class State(value: Option[NormalWinBettingTicketPayload])
+  sealed trait State {
+    def applyCommand(command: Command): Effect[Event, State]
+    def applyEvent(event: Event): State
+  }
 
-  def apply(id: PersistenceId): Behavior[Command] = {
+  final case class InitState(value: Option[NormalWinBettingTicketPayload]) extends State {
+    override def applyCommand(command: Command): Effect[Event, State] = commandHandler(value, command)
+
+    override def applyEvent(event: Event): State = eventHandler(event)
+
+    private def commandHandler(state: Option[NormalWinBettingTicketPayload], command: Command): Effect[Event, State] = {
+      log.info("this status is InitState")
+      command match {
+        case b: BettingInfo => {
+          log.info("receive betting ticket info")
+          log.info(b.normalWinBettingTicketPayload.toString)
+          Effect
+            .persist(Saved(b.normalWinBettingTicketPayload))
+        }
+        case _ =>
+          Effect.unstashAll()
+      }
+    }
+
+    private def eventHandler(event: Event): State = {
+      event match {
+        case s: Saved => Registered(Some(s.normalWinBettingTicketPayload))
+        case _ => throw new IllegalArgumentException()
+      }
+    }
+  }
+
+  final case class Registered(value: Option[NormalWinBettingTicketPayload]) extends State {
+    override def applyCommand(command: Command): Effect[Event, State] = {
+      log.info("this status is Registered")
+      command match {
+        case g: GetBettingInfo =>
+          Effect.reply(g.replyTo)(ResBettingInfo(value))
+        case b: BettingInfo => {
+          log.info("receive betting ticket info")
+          log.info(b.normalWinBettingTicketPayload.toString)
+          Effect
+            .persist(Saved(b.normalWinBettingTicketPayload))
+        }
+        case _ =>
+          Effect.unstashAll()
+      }
+    }
+
+    override def applyEvent(event: Event): State = Registered(value)
+  }
+
+  def apply(id: PersistenceId): Behavior[Command] =
     EventSourcedBehavior[Command, Event, State](
       persistenceId = id,
-      emptyState = State(None),
-      commandHandler = commandHandler,
-      eventHandler = eventHandler
+      emptyState = InitState(None),
+      commandHandler = (state, command) => state.applyCommand(command),
+      eventHandler = (state, event) => state.applyEvent(event)
     )
-  }
-
-  private def commandHandler(state: State, command: Command): Effect[Event, State] = {
-    command match {
-      case b: BettingInfo => {
-        log.info("receive betting ticket info")
-        log.info(b.normalWinBettingTicketPayload.toString)
-        Effect
-          .persist(Saved(b.normalWinBettingTicketPayload))
-          .thenRun { state =>
-            println(state.value)
-          }
-      }
-      case g: GetBettingInfo =>
-        Effect
-          .reply(g.replyTo)(ResBettingInfo(state.value))
-      case _ =>
-        Effect.unstashAll()
-    }
-  }
-
-  private def eventHandler(state: State, event: Event): State = {
-    event match {
-      case s: Saved =>
-        State(Some(s.normalWinBettingTicketPayload))
-    }
-  }
 }
